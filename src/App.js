8127368677a2f8a37a8d5a1b7c9d06a18ab4c618
Soomin Kimu <soominkimu@gmail.com>
2019-06-18 17:16:28 +0900

    SHIHOSHI masonry

--- src/App.js
+++ src/App.js
@@ -1,24 +1,61 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
 
-function App() {
+const MasonryHeader = props =>
+  <div className="masonry-header">
+    <img src="images/shlogo.png" alt="SHIHOSHI Logo" />
+  </div>;
+
+const MasonryItem = props => {
+  return (
+    <div className="masonry-item">
+      <div className="masonry-content">
+        <div className="masonry-img">
+          <a href={props.src} target="_blank" rel="noopener noreferrer">
+            <img src={props.src} alt="SHIHOSHI Snap" />
+          </a>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+const MasonryFooter = props =>
+  <div className="masonry-footer">
+    <p>
+      <a href="www.shihoshi.com" target="_blank" rel="external noopener noreferrer nofollow">SHIHOSHI</a>
+      &nbsp;(C) 2019&nbsp;
+      <a href="admin@shihoshi.com">contact</a>
+    </p>
+  </div>;
+
+// ES2018 property spread notation {...props}
+const App = () => {
+  const Items = [
+    "images/sh01.jpg",
+    "images/sh02.jpg",
+    "images/sh03.jpg",
+    "images/sh04.jpg",
+    "images/sh05.jpg",
+    "images/sh06.jpg",
+    "images/sh07.jpg",
+    "images/sh08.jpg",
+    "images/sh09.jpg",
+    "images/sh10.jpg",
+    "images/sh11.jpg",
+    "images/sh12.jpg",
+  ];
+  //console.log(id, "items");
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <MasonryHeader title="SHIHOSHI" sub="Style" />
+      <div className="masonry-wrapper">
+        <div className="masonry">
+          {Items.map((it, i) => <MasonryItem key={i} src={it} />)}
+        </div>
+      </div>
+      <MasonryFooter />
     </div>
   );
 }
