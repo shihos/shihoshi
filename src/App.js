82964f6fb74810f42f3ff2151059f6317b5c23eb
Soomin Kimu <soominkimu@gmail.com>
2019-06-18 18:08:58 +0900

    Images reorderd

--- src/App.js
+++ src/App.js
@@ -18,18 +18,17 @@ const ShowItem = props => {
 // ES2018 property spread notation {...props}
 const App = () => {
   const Items = [
-    "images/sh01.jpg",
-    "images/sh02.jpg",
-    "images/sh03.jpg",
-    "images/sh04.jpg",
-    "images/sh05.jpg",
-    "images/sh06.jpg",
-    "images/sh07.jpg",
-    "images/sh08.jpg",
-    "images/sh09.jpg",
-    "images/sh10.jpg",
-    "images/sh11.jpg",
-    "images/sh12.jpg",
+    "images/sh08-1.jpg",
+    "images/sh09-2.jpg",
+    "images/sh10-3.jpg",
+    "images/sh11-4.jpg",
+    "images/sh12-5.jpg",
+    "images/sh02-6.jpg",
+    "images/sh03-7.jpg",
+    "images/sh04-8.jpg",
+    "images/sh05-9.jpg",
+    "images/sh06-10.jpg",
+    "images/sh07-11.jpg",
   ];
   //console.log(id, "items");
 
